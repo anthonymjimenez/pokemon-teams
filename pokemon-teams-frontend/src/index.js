ce3d6b6208622258fbf2ed8539200c87100a7ffc
billyott <willrichott@gmail.com>
2020-11-02 13:55:18 -0500

    Fix render trainer cards.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,8 +2,6 @@ const BASE_URL = "http://localhost:3000";
 const TRAINERS_URL = `${BASE_URL}/trainers`;
 const POKEMONS_URL = `${BASE_URL}/pokemons`;
 const main = document.querySelector("main");
-const trainerList = document.createElement("ul")
-main.append(trainerList)
 
 fetch(TRAINERS_URL)
   .then(response => {
@@ -15,20 +13,23 @@ fetch(TRAINERS_URL)
 
 function renderTrainers(data) {
     data.forEach((trainer) => {
-        const li = document.createElement('li')
-        li.innerHTML =`
+        const trainerLi = document.createElement('li')
+        trainerLi.innerHTML =`
             <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
             <button data-trainer-id="${trainer.id}">Add Pokemon</button>
             <ul>               
             </ul>
             </div>
         `
-        trainerList.append(li)
+        const pokemonUl = trainerLi.querySelector("ul")
+        trainer.pokemons.forEach(pokemon => {
+          const pokemonLi = document.createElement("li")
+          pokemonLi.innerHTML = `
+            ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>
+          `
+          pokemonUl.append(pokemonLi)
+        })
+        main.append(trainerLi)
     }) 
 }
-    //   <li>Jacey (Kakuna) <button class="release" data-pokemon-id="140">Release</button></li>
-    // <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
-   // <li>Mittie (Farfetch'd) <button class="release" data-pokemon-id="149">Release</button></li>
-    ///<li>Rosetta (Eevee) <button class="release" data-pokemon-id="150">Release</button></li>
- //   <li>Rod (Beedrill) <button class="release" data-pokemon-id="151">Release</button></li>
  
