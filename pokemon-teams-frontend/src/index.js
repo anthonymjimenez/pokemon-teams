4fbe314792045351a35cb91fa7bf4ef9164077e8
anthonymjimenez <anthonyjim96@gmail.com>
2020-11-02 15:17:33 -0500

    added delete

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -5,77 +5,77 @@ const main = document.querySelector("main");
 
 //************* initialize *************
 fetch(TRAINERS_URL)
-  .then(response => {
-    return response.json()
+  .then((response) => {
+    return response.json();
   })
-  .then(data => {
-    return renderTrainers(data)
+  .then((data) => {
+    return renderTrainers(data);
   });
-
 
 //************* functions *************
 function renderTrainers(data) {
-    data.forEach((trainer) => {
-        const trainerLi = document.createElement('li')
-        trainerLi.innerHTML =`
+  data.forEach((trainer) => {
+    const trainerLi = document.createElement("li");
+    trainerLi.innerHTML = `
             <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
             <button class="add" data-trainer-id="${trainer.id}">Add Pokemon</button>
             <ul>               
             </ul>
             </div>
-        `
-        const pokemonUl = trainerLi.querySelector("ul")
-        trainer.pokemons.forEach(pokemon => {
-          const pokemonLi = document.createElement("li")
-          pokemonLi.innerHTML = `
+        `;
+    const pokemonUl = trainerLi.querySelector("ul");
+    trainer.pokemons.forEach((pokemon) => {
+      const pokemonLi = document.createElement("li");
+      pokemonLi.innerHTML = `
           ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>
-          `
-          pokemonUl.append(pokemonLi)
-        })
-        trainerLi.dataset.pokemonCount = pokemonUl.children.length
-        main.append(trainerLi)
-    }) 
+          `;
+      pokemonUl.append(pokemonLi);
+    });
+    main.append(trainerLi);
+  });
 }
 
 function appendPokemonLi(trainerLi, pokemon) {
-  const pokemonUl = trainerLi.querySelector('ul')
-  const pokemonLi = document.createElement("li")
+  const pokemonUl = trainerLi.querySelector("ul");
+  const pokemonLi = document.createElement("li");
   pokemonLi.innerHTML = `
     ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>
-    `
-  pokemonUl.append(pokemonLi)
+    `;
+  pokemonUl.append(pokemonLi);
 }
 
 //************* event listeners *************
-main.addEventListener('click', function(e) {
+main.addEventListener("click", function (e) {
   if (e.target.matches("button.add")) {
     const configObj = {
       method: "POST",
       headers: {
-      'Content-Type': 'application/json'
-    },
-      body: JSON.stringify({trainer_id: e.target.dataset.trainerId})
-    }
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ trainer_id: e.target.dataset.trainerId }),
+    };
     fetch(POKEMONS_URL, configObj)
-    .then(response => response.json())
-    .then(pokemon => {
-      if (pokemon.trainer_id) {
-        const trainerCard = document.querySelector(`[data-id="${pokemon.trainer_id}"]`)
-        const pokemonUl = trainerCard.querySelector("ul")
-        appendPokemonLi(trainerCard,pokemon)
-      }
-    })
+      .then((response) => response.json())
+      .then((pokemon) => {
+        if (pokemon.trainer_id) {
+          const trainerCard = document.querySelector(
+            `[data-id="${pokemon.trainer_id}"]`
+          );
+          const pokemonUl = trainerCard.querySelector("ul");
+          appendPokemonLi(trainerCard, pokemon);
+        }
+      });
   }
-})
+});
 
-main.addEventListener('click', function(e) {
+main.addEventListener("click", function (e) {
   if (e.target.matches("button.release")) {
-    fetch(POKEMONS_URL+`/${e.target.dataset.pokemonId}`, {method:"DELETE"})
-    .then(response => json.())
-    .then(data => console.log("Success",data))
+    fetch(POKEMONS_URL + `/${e.target.dataset.pokemonId}`, {
+      method: "DELETE",
+    }).then((response) => {
+      e.target.parentNode.remove();
+      console.log("Removed", response);
+    });
   }
-
-})
-
-
+});
 
